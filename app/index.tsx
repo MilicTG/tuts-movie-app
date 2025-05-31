922dded6f1a9abdc4e281d97b7c921b5dd20a3cb
Ivan Milic <milic.tg@gmail.com>
2025-05-31 12:13:10 +0200

    finaly nativewind is working

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex flex-1 flex-col justify-center items-center">
-      <Text>Welcome</Text>
+    <View className="flex-1 justify-center items-center bg-blue-50">
+      <Text className="text-dark-200 text-xl">This text should be styled</Text>
     </View>
   );
 }
