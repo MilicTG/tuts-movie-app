4947e3d9ff6a55fc9282d54aa27f9a52e1175918
Ivan Milic <milic.tg@gmail.com>
2025-05-31 15:10:21 +0200

    started with navbar

---
+++ app/movie/[id].tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function MovieDetails() {
+  return (
+    <View>
+      <Text>Movie details</Text>
+    </View>
+  );
+}
+
+export default MovieDetails;
